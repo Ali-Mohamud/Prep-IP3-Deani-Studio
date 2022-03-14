26c3bffd5c1122d26d7292aec39e272dfa63913b
Ali-Mohamud <ali.mohamud@student.moringaschool.com>
2022-03-14 03:27:47 +0300

    feat: add hover effect to portfolio

--- js/script.js
+++ js/script.js
@@ -28,4 +28,108 @@ $(document).ready(function () {
         $("#design-3").toggle();
         $(".hide-product-management").toggle();
     })
+
+
+
+    $("#hover").hover(function () {
+        $(this).animate({
+            opacity: '0.3'
+            
+        });
+    },
+    function () {
+        $(this).animate({
+            opacity: '1' 
+        });
+    })
+
+
+
+$("#hover-1").hover(function () {
+    $(this).animate({
+        opacity: '0.3'
+        
+    });
+},
+function () {
+    $(this).animate({
+        opacity: '1' 
+    });
+})
+
+
+$("#hover-2").hover(function () {
+    $(this).animate({
+        opacity: '0.3'
+        
+    });
+},
+function () {
+    $(this).animate({
+        opacity: '1' 
+    });
+})
+
+$("#hover-3").hover(function () {
+    $(this).animate({
+        opacity: '0.3'
+        
+    });
+},
+function () {
+    $(this).animate({
+        opacity: '1' 
+    });
+})
+
+
+$("#hover-4").hover(function () {
+    $(this).animate({
+        opacity: '0.3'
+        
+    });
+},
+function () {
+    $(this).animate({
+        opacity: '1' 
+    });
+})
+
+
+$("#hover-5").hover(function () {
+    $(this).animate({
+        opacity: '0.3'
+        
+    });
+},
+function () {
+    $(this).animate({
+        opacity: '1' 
+    });
+})
+
+
+$("#hover-6").hover(function () {
+    $(this).animate({
+        opacity: '0.3'
+        
+    });
+},
+function () {
+    $(this).animate({
+        opacity: '1' 
+    });
+})
+
+$("#hover-7").hover(function () {
+    $(this).animate({
+        opacity: '0.3'
+        
+    });
+},
+function () {
+    $(this).animate({
+        opacity: '1' 
+    });
+})
 });
