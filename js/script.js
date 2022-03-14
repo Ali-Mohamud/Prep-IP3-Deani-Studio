752d8c674cdad76fde6af257d3220afdd4e6b4da
Ali-Mohamud <ali.mohamud@student.moringaschool.com>
2022-03-14 03:24:57 +0300

    feat: add toggle to show text of icons

--- js/script.js
+++ js/script.js
@@ -9,3 +9,23 @@ $(function () {
 $(function () {
     $("#design-3").hide();
 })
+
+
+$(document).ready(function () {
+
+    $("#design").click(function () {
+        $(".hide-design-image").toggle();
+        $("#design-1").toggle();
+
+    })
+
+    $("#development").click(function () {
+        $("#design-2").toggle();
+        $(".hide-development-image").toggle();
+    })
+
+    $("#productManagement").click(function () {
+        $("#design-3").toggle();
+        $(".hide-product-management").toggle();
+    })
+});
